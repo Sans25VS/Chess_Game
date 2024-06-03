acb83228e3eb284d03b8d33af2b02145b570c588
sanskar <sanskargupta37081@gmail.com>
2024-06-03 14:32:30 +0530

    Basic backend Completed handled all the invalid moves and also quiting of a player

--- app.js
+++ app.js
@@ -16,9 +16,54 @@ app.use(express.static(path.join(__dirname,"public")));
 app.get("/",(req,res) =>{
     res.render("index",{title:"Welcome to Chess Game"});
 });
+// callback function which tells how many players are connected right now
 io.on("connection",function(uniquesocket){
     console.log("Connected");
+uniquesocket.on("Disconnected",function(){
+    console.log("Disconnected");
 })
+});
+if(!players.white){
+    players.white = uniquesocket.id;
+    uniquesocket.emit("playerRole","w");
+}
+else if(!players.black){
+    players.black = uniquesocket.id;
+    uniquesocket.emit("playerRole","b");
+}
+else{
+    uniquesocket.emit("spectatorRole");
+
+}
+uniqusocket.on("disconnect", function(){
+    if(uniquesocket.id === players.white){
+        delete players.white;
+    }
+    else if (uniquesocket.id === players.black){
+        delete players.black;
+    }
+});
+uniquesocket.on("move",(move)=>{
+    try{
+        if(chess.turn() === "w" && uniquesocket.id !== players.white) return;
+        if(chess.turn() === "b" && uniquesocket.id !== players.black) return;
+        const result = chess.move(move);
+        if(result){
+            currentPlayer = chess.turn();
+            io.emit("move",move);
+            io.emit("boardState", chess.fen());
+        }
+        else{
+            console.log("Invalid Move :", move);
+            uniquesocket.emit("invalidmove", move);
+        }
+    }
+    catch(err){
+        console.log(err);
+        console.log("Invalid Move :", move);
+    }
+});
+
 server.listen(3000,function(){
     console.log("Server running on port 3000");
 });
